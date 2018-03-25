8f3922af3fb81222cdef5f366f3fa88d9ba8b92c
AntoniBellver <33597124+AntoniBellver@users.noreply.github.com>
2018-03-25 17:33:39 +0200

    PlayerMovement_02
    Camera Follow, Grid de numeros, intento de disparo 1

---
+++ Sigma_game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target; //Posición que la cámara seguirá durante la ejecución del juego
+    public float smoothing = 5f;
+
+    private Vector3 offset;
+
+   
+
+	void Start ()
+    {
+        //Calculamos el offset inicial
+        offset = transform.position - target.position;
+      
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset; //Crea una posición a la que apunta la cámara basado en el offset del target
+
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+	
+}
---
+++ Sigma_game/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    public float shotSpeed = 1.5f;
+
+    private Rigidbody m_rigidbody;
+
+    void Awake()
+    {
+        m_rigidbody.GetComponent<Rigidbody>();
+        m_rigidbody.velocity = transform.forward * shotSpeed;
+    }
+
+   /* void Start()
+    {
+        m_rigidbody.GetComponent<Rigidbody>();
+        m_rigidbody.velocity = transform.forward * shotSpeed;
+    }*/
+
+}
